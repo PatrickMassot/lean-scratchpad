96290fd73bd3d46da75ee19ce286c21121a7e67c
Patrick Massot <patrickmassot@free.fr>
2018-05-28 21:45:59 +0200

    Still suffering

--- src/support.lean
+++ src/support.lean
@@ -114,6 +114,22 @@ end
 
 local notation `〚`a, b`〛` := comm a b  -- type with \llb / \rrb
 
+lemma numbers' {N} (H : N > 0) : ∀ i, i ∈ range' 0 (N - 1 + 1 - 0) → N - (N - 1 - i) = i + 1 :=
+begin
+  intros i hyp,
+  simp [nat.add_sub_cancel' H] at hyp,
+  rw [nat.sub_sub, add_comm, nat.sub_sub_self hyp.2]
+end
+
+/- replace hyp := hyp.right,
+  have := nat.succ_le_of_lt  hyp,
+  
+  rw [← int.coe_nat_inj', int.coe_nat_sub, int.coe_nat_sub, int.coe_nat_sub],
+  { ring },
+  { assumption },
+  { sorry },
+  { sorry }-/
+
 
 lemma trading_of_displaced (g a b : homeo X X) 
   (supp_hyp : supp a ∩ g '' supp b = ∅) :
@@ -123,12 +139,15 @@ begin
   rw ←supp_conj at supp_hyp,
   rw [commuting, fundamental supp_hyp]
 end
-set_option pp.coercions true
+
+--set_option trace.class_instances true
+--set_option pp.all true
+
 lemma commutators_crunching (U : set X) (φ f : homeo X X)
 (wandering_hyp : ∀ i j : ℕ, i ≠ j → ⇑(φ^i) '' U ∩ ⇑(φ^j) '' U = ∅)
-(N : ℕ) (a : ℕ → homeo X X) (b : ℕ → homeo X X) 
-(supp_hyp : ∀ k : ℕ, supp (a k) ⊆ U ∧ supp (b k) ⊆ U)
-(comm_hyp : f = Π_(i = 1..N) 〚a i, b i〛) :
+(N : ℕ) (aa : ℕ → homeo X X) (b : ℕ → homeo X X) 
+(supp_hyp : ∀ k : ℕ, supp (aa k) ⊆ U ∧ supp (b k) ⊆ U)
+(comm_hyp : f = Π_(i = 1..N) 〚aa i, b i〛) :
 ∃ A B C D : homeo X X, f = 〚A, B〛 * 〚C, D〛 := 
 begin
   by_cases H : N = 0,
@@ -138,8 +157,24 @@ begin
     simp [H, zero_lt_one] },
   { replace H : N ≥ 1 := nat.succ_le_of_lt (nat.pos_of_ne_zero H),
     let G := homeo X X,
-    let g := λ i, (Π_(j=1..i) 〚a i, b i〛: G),
+    let g := λ i, (Π_(j=1..i) 〚aa i, b i〛: G),
     let F := (Π_(i=0..(N-1)) conj (φ^i) $ g (N-i) : G),
+
+    have commute : ∀ (i j k l : ℕ), i ≠ j → 
+      (conj (φ^i) (aa k))*(conj (φ^j) (b l)) = (conj (φ^j) (b l))*(conj (φ^i) (aa k)),
+    begin
+      intros i j k l i_neq_j,
+      apply fundamental,
+      repeat { rw supp_conj },
+      specialize wandering_hyp i j i_neq_j,
+      have supp_a := set.image_subset (φ^i : G) (supp_hyp k).left,
+      have supp_b := set.image_subset (φ^j : G)  (supp_hyp l).right,
+      
+      have := set.inter_subset_inter supp_a supp_b,
+      rw wandering_hyp at this,
+      exact set.eq_empty_of_subset_empty this
+    end,
+
     have numbers := 
     calc 1 + (1 + (N - 1)) - 1 =  (1 + (N-1)) + 1 - 1 : by simp 
     ... = 1 + (N-1) : by rw nat.add_sub_cancel
@@ -151,30 +186,25 @@ begin
     ... = Π_(i=0..(N-1)) conj (φ^(i+1)) $ g (N-i) : by simp[pow_succ]
     ... = Π_(i=1..N) conj (φ^i) $ g (N-(i-1)) : by { rw big.shift _ _ _ _ 0 (N-1) 1, simp, rw numbers, apply big.ext, simp, intros i hyp, rw nat.add_sub_cancel' (mem_range'.1 hyp).left},
 
-    have := inv_is_group_anti_hom.mul,
-    
-    have numbers' : ∀ i, i ∈ range' 0 (N - 1 + 1 - 0) → N - (N - 1 - i) = i + 1 :=
-    begin
-      intros i hyp,
-      simp [nat.add_sub_cancel' H] at hyp,
-      --rw [nat.sub_sub, add_comm, nat.sub_sub_self hyp.2],
-      replace hyp := hyp.right,
-      have := nat.succ_le_of_lt  hyp,
-      
-      rw [← int.coe_nat_inj', int.coe_nat_sub, int.coe_nat_sub, int.coe_nat_sub],
-      { ring },
-      { assumption },
-      { sorry },
-      { sorry }
-    end,
-
+    have := (@inv_is_group_anti_hom G _ _).mul,
+    let gg := λ i, (conj (φ^i) $ (g (N-i))⁻¹ : G),
     have Finv := calc
     F⁻¹ = (Π_(i=0..(N-1)) conj (φ^i) $ g (N-i) : G)⁻¹ : rfl
     ... = (λ (g : G), g⁻¹) (Π_(i=0..(N-1)) conj (φ^i) $ g (N-i) : G) : rfl
     ... = (Π_(i=0..(N-1)) (conj (φ^(N-1-i)) $ g (N - (N - 1 - i)))⁻¹ : G) : by { rw big.range_anti_mph _ (1:G) _ _ _ _ this ; simp }
-    ... = (Π_(i=0..(N-1)) (conj (φ^(N-1-i)) $ g (i+1))⁻¹ : G) : by { apply big.ext, { simp }, intros i hyp, rw numbers' i hyp },
-    sorry,
-    apply_instance }
+    ... = (Π_(i=0..(N-1)) (conj (φ^(N-1-i)) $ g (i+1))⁻¹ : G) : by { apply big.ext, { simp }, intros i hyp, rw numbers' H i hyp }
+    ... = (Π_(i=0..(N-1)) conj (φ^(N-1-i)) $ (g (i+1))⁻¹ : G) : by { apply big.ext, simp, intros, rw ←is_group_hom.inv (conj _), apply_instance }
+    ... = (Π_(i=0..(N-1)) conj (φ^i) $ (g (N-i))⁻¹ : G) : by { rw big.reverse_range_of_commute, apply big.ext, simp, simp, sorry, sorry }
+    ... = (Π_(i=0..(N-1)) gg i)*(gg N)  : by { dsimp [g], simp[big.nil], sorry }
+    ... = (Π_(i=0..N) gg i) : by{  have := (big.concat_true (*) (1:G) (range' 0 (N - 1 + 1 - 0)) gg N), cc, },
+
+    
+    have : conj (φ^N) (g 0)⁻¹ = 1,
+    by { dsimp [g], simp[big.nil] } ,
+    
+
+    sorry }
 end
 
+#check big.concat_true
 #check int.cast_inj
